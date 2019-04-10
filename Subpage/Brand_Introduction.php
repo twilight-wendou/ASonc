44132975563518801fbb9b4b1ae7cd66dc51b907
Twilight-Z <913679322@qq.com>
2019-04-10 12:29:11 +0800

    头部导航栏修改

--- Subpage/Brand_Introduction.php
+++ Subpage/Brand_Introduction.php
@@ -17,13 +17,13 @@
 <title>ASonc Brand Introduction</title>
 
 <!-- 引入头文件 head -->
-<?php include_once('../HTML/head.html'); ?>
+<?php include('../HTML/head.html'); ?>
 <!-- 引入单独样式文件 -->
 <!-- <link rel="stylesheet" href="./CSS/xxx.css" type="text/css" media="All"> -->
 
 
 <!-- 引入头部文件 header -->
-<?php include_once('../HTML/header.html'); ?>
+<?php include('../HTML/header.html'); ?>
 
 
 <!-- 引入单独HTML文件 body -->
