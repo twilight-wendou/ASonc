4883ed0e57fbf1e0282cf7c82842973fcc8716bb
Twilight-Z <913679322@qq.com>
2019-04-11 11:52:11 +0800

    主页CSS修改

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * @Author: Twilight 
  * @Date: 2019-04-08 11:24:41 
  * @Last Modified by: Twilight
- * @Last Modified time: 2019-04-08 12:33:34
+ * @Last Modified time: 2019-04-11 11:49:55
  */
 /*************** 艾森首页 页面 ****************/
    header("content-type: text/html; charset=utf8");/* 头文件设置 */
@@ -26,7 +26,7 @@
 
 
 <!-- 引入单独HTML文件 body -->
-<?php include_once('./HTML/main.html'); ?>
+<?php include_once('./HTML/index_border.html'); ?>
 
 
 <!-- 引入脚部文件 footer -->
