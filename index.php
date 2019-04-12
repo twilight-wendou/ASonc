1eb4606c90256bd28d0b5585a0ca3d2b57d41963
Twilight-Z <913679322@qq.com>
2019-04-12 11:42:31 +0800

    头尾部、首页修改

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * @Author: Twilight 
  * @Date: 2019-04-08 11:24:41 
  * @Last Modified by: Twilight
- * @Last Modified time: 2019-04-11 11:49:55
+ * @Last Modified time: 2019-04-11 12:30:31
  */
 /*************** 艾森首页 页面 ****************/
    header("content-type: text/html; charset=utf8");/* 头文件设置 */
@@ -18,7 +18,7 @@
 <!-- 引入头文件 head -->
 <?php include_once('./HTML/head.html'); ?>
 <!-- 引入单独样式文件 -->
-<!-- <link rel="stylesheet" href="./CSS/xxx.css" type="text/css" media="All"> -->
+<link rel="stylesheet" href="./CSS/index.css" type="text/css" media="All">
 
 
 <!-- 引入头部文件 header -->
