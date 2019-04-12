dabc8954c9e0b5c661ba6f0df20043e9ba775c96
Twilight-Z <913679322@qq.com>
2019-04-12 18:26:01 +0800

    路径修改

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * @Author: Twilight 
  * @Date: 2019-04-08 11:24:41 
  * @Last Modified by: Twilight
- * @Last Modified time: 2019-04-11 12:30:31
+ * @Last Modified time: 2019-04-12 18:18:53
  */
 /*************** 艾森首页 页面 ****************/
    header("content-type: text/html; charset=utf8");/* 头文件设置 */
@@ -14,18 +14,18 @@
 <title>ASonc</title>
 
 <!-- 引入头文件 head -->
-<?php include_once('./HTML/head.html'); ?>
+<?php include_once('./View/head.html'); ?>
 <!-- 引入单独样式文件 -->
-<link rel="stylesheet" href="./CSS/index.css" type="text/css" media="All">
+<link rel="stylesheet" href="./Static/CSS/index.css" type="text/css" media="All">
 
 
 <!-- 引入头部文件 header -->
-<?php include_once('./HTML/header.html'); ?>
+<?php include_once('./View/header.html'); ?>
 
 
 <!-- 引入单独HTML文件 body -->
-<?php include_once('./HTML/index_border.html'); ?>
+<?php include_once('./View/index_border.html'); ?>
 
 
 <!-- 引入脚部文件 footer -->
-<?php include_once('./HTML/foot.html'); ?>
+<?php include_once('./View/foot.html'); ?>
