74227da67aa2035b1b02744500d9eed7b1087e6e
ljcyulong <1490586120@qq.com>
2019-04-12 23:41:00 +0800

    整合修改

--- Project_Case.php
+++ Project_Case.php
@@ -15,18 +15,19 @@
 <title>ASonc Project Case</title>
 
 <!-- 引入头文件 head -->
-<?php include_once('../HTML/head.html'); ?>
+<?php include_once('View/head.html'); ?>
 <!-- 引入单独样式文件 -->
-<!-- <link rel="stylesheet" href="./CSS/xxx.css" type="text/css" media="All"> -->
+<link rel="stylesheet" href="./Static/CSS/case.css" type="text/css" media="All">
 
 
 <!-- 引入头部文件 header -->
-<?php include_once('../HTML/header.html'); ?>
+<?php include_once('View/header.html'); ?>
 
 
 <!-- 引入单独HTML文件 body -->
-<?php include_once('../HTML/case.html'); ?>
+<?php include_once('View/case.html'); ?>
 
 
 <!-- 引入脚部文件 footer -->
-<?php include_once('../HTML/foot.html'); ?>
+<?php include_once('View/foot.html'); ?>
+<script src="./Static/JS/case.js"></script>
