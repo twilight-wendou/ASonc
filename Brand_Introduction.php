90ee4ee05b1dc22c33b1f08a34fee58115305a76
zhang2855490142 <2855490142@qq.com>
2019-04-15 11:46:33 +0800

    品牌简介

--- Brand_Introduction.php
+++ Brand_Introduction.php
@@ -12,12 +12,13 @@
 
 
 <!-- 网页名 -->
-<title>ASonc Brand Introduction</title>
+<title>品牌简介</title>
 
 <!-- 引入头文件 head -->
 <?php include('./View/head.html'); ?>
 <!-- 引入单独样式文件 -->
-<!-- <link rel="stylesheet" href="./CSS/xxx.css" type="text/css" media="All"> -->
+<link rel="stylesheet" href="./static/CSS/recruitment.css" type="text/css" media="All">
+<link rel="stylesheet" href="./Static/CSS/brand.css" type="text/css" media="All">
 
 
 <!-- 引入头部文件 header -->
@@ -25,7 +26,7 @@
 
 
 <!-- 引入单独HTML文件 body -->
-<!-- <?php include_once('./View/xxx.html'); ?> -->
+<!-- <?php include_once('./View/brand.html'); ?> -->
 
 
 <!-- 引入脚部文件 footer -->
--- Talent_Recruitment.php
+++ Talent_Recruitment.php
@@ -12,12 +12,12 @@
 
 
 <!-- 网页名 -->
-<title>ASonc Talent Recruitment</title>
+<title>人才招聘</title>
 
 <!-- 引入头文件 head -->
 <?php include_once('./View/head.html'); ?>
 <!-- 引入单独样式文件 -->
-<!-- <link rel="stylesheet" href="./CSS/xxx.css" type="text/css" media="All"> -->
+<link rel="stylesheet" href="./static/CSS/recruitment.css" type="text/css" media="All">
 
 
 <!-- 引入头部文件 header -->
@@ -25,7 +25,7 @@
 
 
 <!-- 引入单独HTML文件 body -->
-<!-- <?php include_once('./View/xxx.html'); ?> -->
+<!-- <?php include_once('./View/recruitment.html'); ?> -->
 
 
 <!-- 引入脚部文件 footer -->
