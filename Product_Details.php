bf4e5b2c08ecf55284e40550f485d64696996dca
helloo <1624556443@qq.com>
2019-04-15 11:44:05 +0800

    产品详情页修改

--- Product_Details.php
+++ Product_Details.php
@@ -18,14 +18,14 @@
 <?php include_once('./View/head.html'); ?>
 <!-- 引入单独样式文件 -->
 <!-- <link rel="stylesheet" href="./CSS/xxx.css" type="text/css" media="All"> -->
-
+<link rel="stylesheet" href="./Static/CSS/Product_Details.css">
 
 <!-- 引入头部文件 header -->
 <?php include_once('./View/header.html'); ?>
 
 
 <!-- 引入单独HTML文件 body -->
-<?php //echo '<link rel="stylesheet" href="./Static/CSS/Product_Details.css">' ?>
+
 <!-- <?php include_once('./View/Product_Details.html'); ?> -->
 
 
