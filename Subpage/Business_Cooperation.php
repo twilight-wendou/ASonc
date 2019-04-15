b691ac0576d6a1cc14d21e6afd85da4432579937
Xue Wenyi <745157409@qq.com>
2019-04-15 09:12:47 +0800

    two

--- Subpage/Business_Cooperation.php
+++ Subpage/Business_Cooperation.php
@@ -19,7 +19,7 @@
 <!-- 引入头文件 head -->
 <?php include_once('../HTML/head.html'); ?>
 <!-- 引入单独样式文件 -->
-<!-- <link rel="stylesheet" href="./CSS/xxx.css" type="text/css" media="All"> -->
+<link rel="stylesheet" href="./CSS/bus_cooperat.css" type="text/css" media="All">
 
 
 <!-- 引入头部文件 header -->
@@ -27,7 +27,7 @@
 
 
 <!-- 引入单独HTML文件 body -->
-<!-- <?php include_once('../HTML/xxx.html'); ?> -->
+<?php include_once('../HTML/bus_cooperat.html'); ?>
 
 
 <!-- 引入脚部文件 footer -->
