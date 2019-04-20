e84fcdb05912807c963ec06f2af574da473adcf8
Twilight-Z <913679322@qq.com>
2019-04-20 15:34:10 +0800

    更新css、js

--- Static/JS/index.js
+++ Static/JS/index.js
@@ -1,1 +1,13 @@
-// 主页js
+/*
+ * @Author: Twilight 
+ * @Date: 2019-04-17 11:53:45 
+ * @Last Modified by: Twilight
+ * @Last Modified time: 2019-04-17 11:58:53
+ */
+/* 主页js */
+// 层叠覆盖动画
+   // 1、获取对象
+   const banner = $('#ibox .jumbotron');
+
+   // 2、初始化
+   // 3、覆盖动画
