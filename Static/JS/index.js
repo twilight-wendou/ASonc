93bcb6586665f24a82dd03b9960afd4c34084cec
Twilight-Z <913679322@qq.com>
2019-04-22 10:27:51 +0800

    字体修改

--- Static/JS/index.js
+++ Static/JS/index.js
@@ -2,12 +2,12 @@
  * @Author: Twilight 
  * @Date: 2019-04-17 11:53:45 
  * @Last Modified by: Twilight
- * @Last Modified time: 2019-04-17 11:58:53
+ * @Last Modified time: 2019-04-22 10:05:21
  */
 /* 主页js */
 // 层叠覆盖动画
    // 1、获取对象
-   const banner = $('#ibox .jumbotron');
+   // const banner = $('#ibox .jumbotron');
 
    // 2、初始化
    // 3、覆盖动画
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * @Author: Twilight 
  * @Date: 2019-04-08 11:24:41 
  * @Last Modified by: Twilight
- * @Last Modified time: 2019-04-15 12:07:42
+ * @Last Modified time: 2019-04-22 10:20:14
  */
 /*************** 艾森首页 页面 ****************/
    header("content-type: text/html; charset=utf8");/* 头文件设置 */
@@ -30,3 +30,26 @@
 
 <!-- 引入脚部文件 footer -->
 <?php include_once('./View/Common/foot.html'); ?>
+
+
+<script>
+    // 字体自适应 Start
+   var width_srceen = screen.width;
+   var font_size = "";
+   window.onresize = function () {
+      changbody_fontSize(".chang_fontSize");
+   }
+   window.onload = function () {
+      $("html").css("-webkit-text-size-adjust", "none");
+      font_size = $(".chang_fontSize").css("font-size").split("px")[0];
+      changbody_fontSize();
+   }
+
+   function changbody_fontSize(obj) {
+      var new_window_width = $(window).width();
+      var multiple = new_window_width / width_srceen;
+      var hi = font_size * multiple;
+      $(".chang_fontSize").css("font-size", hi + "px");
+   }
+    // 字体自适应 END
+</script>
